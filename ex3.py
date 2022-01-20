72e417fb24d7824166cb2f1c072187eb46431a32
Dvir Twito <dvir019@gmail.com>
2022-01-20 21:08:04 +0200

    Convert Q into dictionary

--- ex3.py
+++ ex3.py
@@ -2,6 +2,8 @@ import numpy as np
 import random
 
 DRONE_LOCATION = "drone_location"
+TARGET_LOCATION = "target_location"
+DRONE = "drone"
 RESET = "reset"
 WAIT = "wait"
 PICK = "pick"
@@ -31,13 +33,15 @@ class DroneAgent:
         self.mode = 'train'  # do not change this!
 
         self.actions = [RESET, WAIT, PICK, MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, DELIVER]
+
+        self.learning_actions = [MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, RESET]
+
         # TODO: maybe increase the state to include number of packages
         self.current_packages_on_drone = 0
-        self.visited = set()
+        # self.visited = set()
         self.current_round = 0
 
-        self.q_values = np.zeros((n, m,NUM_PARTS_OF_EPISODE, NUM_PACKAGES_DRONE + 1, 2,
-                                  len(self.actions)))
+        self.q_values = {}
         self.action_num_dict = self.create_action_num_dict()
         self.num_action_dict = self.create_num_action_dict()
 
@@ -47,17 +51,15 @@ class DroneAgent:
         if self.current_round < 20:
             return MIDDLE_OF_EPISODE
         return END_OF_EPISODE
+
     def create_action_num_dict(self):
         # Create a dictionary that converts an action to a number for np.array access
         return {
-            RESET: 0,
-            WAIT: 1,
-            PICK: 2,
-            MOVE_UP: 3,
-            MOVE_DOWN: 4,
-            MOVE_LEFT: 5,
-            MOVE_RIGHT: 6,
-            DELIVER: 7
+            MOVE_UP: 0,
+            MOVE_DOWN: 1,
+            MOVE_LEFT: 2,
+            MOVE_RIGHT: 3,
+            RESET: 4
         }
 
     def package_exists_on_drone_location(self, obs):
@@ -73,27 +75,35 @@ class DroneAgent:
     def create_num_action_dict(self):
         # Create a dictionary that converts a number to an action string
         return {
-            0: RESET,
-            1: WAIT,
-            2: PICK,
-            3: MOVE_UP,
-            4: MOVE_DOWN,
-            5: MOVE_LEFT,
-            6: MOVE_RIGHT,
-            7: DELIVER
+            0: MOVE_UP,
+            1: MOVE_DOWN,
+            2: MOVE_LEFT,
+            3: MOVE_RIGHT,
+            4: RESET
         }
 
     def select_action(self, obs0):
+        print(obs0)
         # TODO: maybe implement differently between train and eval
         if not obs0[PACKAGES]:
             return RESET
-        obs_location_x, obs_location_y = obs0[DRONE_LOCATION]
-        num_packages_on_drone = self.get_packages_on_drone(obs0)
-        is_package_exists = self.package_exists_on_drone_location(obs0)
 
-        state_obs = (obs_location_x, obs_location_y, num_packages_on_drone)
-        q_values = self.q_values[
-            obs_location_x, obs_location_y,self.get_part_of_episode(), num_packages_on_drone, is_package_exists]
+        if self.can_deliver(obs0):
+            return DELIVER
+
+        if self.can_pick(obs0):
+            return PICK
+        # obs_location_x, obs_location_y = obs0[DRONE_LOCATION]
+        # num_packages_on_drone = self.get_packages_on_drone(obs0)
+        # is_package_exists = self.package_exists_on_drone_location(obs0)
+
+        # state_obs = (obs_location_x, obs_location_y, num_packages_on_drone)
+
+        obs_as_key = self.obs_to_key(obs0)
+
+        self.add_obs_if_not_exists(obs_as_key)
+
+        q_values = self.q_values[obs_as_key]
 
         if self.mode == 'train' and (random.uniform(0, 1) < random_action_prob):
             if 0 in q_values:
@@ -115,42 +125,80 @@ class DroneAgent:
 
     def update(self, obs0, action, obs1, reward):
         # TODO: maybe convert the obs dictionary to a string with pickle
-        if action == DELIVER and reward == -1:
+        if action == DELIVER and reward == -1:  # Illegal deliver
             reward = -20
 
         if reward == 100:
             reward *= 100
 
-        obs0_location_x, obs0_location_y = obs0[DRONE_LOCATION]
-        obs1_location_x, obs1_location_y = obs1[DRONE_LOCATION]
+        # obs0_location_x, obs0_location_y = obs0[DRONE_LOCATION]
+        # obs1_location_x, obs1_location_y = obs1[DRONE_LOCATION]
 
-        obs0_packages_on_drone = self.get_packages_on_drone(obs0)
-        obs1_packages_on_drone = self.get_packages_on_drone(obs1)
+        # obs0_packages_on_drone = self.get_packages_on_drone(obs0)
+        # obs1_packages_on_drone = self.get_packages_on_drone(obs1)
 
-        current_part_of_episode = self.get_part_of_episode()
+        # current_part_of_episode = self.get_part_of_episode()
         self.current_round += 1
-        next_part_of_episode = self.get_part_of_episode()
+        # next_part_of_episode = self.get_part_of_episode()
 
-        is_package_exists_obs0 = self.package_exists_on_drone_location(obs0)
-        is_package_exists_obs1 = self.package_exists_on_drone_location(obs1)
+        # is_package_exists_obs0 = self.package_exists_on_drone_location(obs0)
+        # is_package_exists_obs1 = self.package_exists_on_drone_location(obs1)
 
-        visited_observation = (obs0_location_x, obs0_location_y,current_part_of_episode,
-                               obs0_packages_on_drone)
-        self.visited.add(visited_observation)
-        action_index = self.action_num_dict[action]
+        # visited_observation = (obs0_location_x, obs0_location_y, current_part_of_episode,
+        #                        obs0_packages_on_drone)
+        # self.visited.add(visited_observation)
 
-        old_q_value = self.q_values[obs0_location_x, obs0_location_y,current_part_of_episode,
-                                    obs0_packages_on_drone,
-                                    is_package_exists_obs0, action_index]
-        td_value = reward + GAMMA * np.max(
-            self.q_values[
-                obs1_location_x, obs1_location_y,next_part_of_episode, obs1_packages_on_drone,
-                is_package_exists_obs1]) - old_q_value
+        if action in self.learning_actions:
+            action_index = self.action_num_dict[action]
 
-        self.q_values[obs0_location_x, obs0_location_y,current_part_of_episode,
-                      obs0_packages_on_drone,
-                      is_package_exists_obs0, action_index] = (
-                old_q_value + ALPHA * td_value)
+            obs0_as_key = self.obs_to_key(obs0)
+
+            obs1_as_key = self.obs_to_key(obs1)
+
+            old_q_value = self.q_values[obs0_as_key]
+
+            self.add_obs_if_not_exists(obs1_as_key)
+
+            td_value = reward + GAMMA * np.max(
+                self.q_values[obs1_as_key]) - old_q_value
+
+            self.q_values[obs0_as_key] = (
+                    old_q_value + ALPHA * td_value)
 
         if self.current_round == 29 or action == RESET:
             self.current_round = 0
+
+    def can_deliver(self, obs):
+        drone_location = obs[DRONE_LOCATION]
+        target_location = obs[TARGET_LOCATION]
+        if drone_location == target_location:
+            packages = obs[PACKAGES]
+            for package, package_location in packages:
+                if package_location == DRONE:
+                    return True
+        return False
+
+    def can_pick(self, obs):
+        drone_location = obs[DRONE_LOCATION]
+        packages_on_drone = 0
+        pickable_packages = 0
+
+        packages = obs[PACKAGES]
+        for package, package_location in packages:
+            if package_location == DRONE:
+                packages_on_drone += 1
+            elif package_location == drone_location:
+                pickable_packages += 1
+
+        return packages_on_drone < 2 and pickable_packages > 0
+
+
+    def obs_to_key(self, obs):
+        '''
+        Turns an observation into string key
+        '''
+        return str(obs)
+
+    def add_obs_if_not_exists(self, obs_as_key):
+        if obs_as_key not in self.q_values:
+            self.q_values[obs_as_key] = np.zeros(len(self.learning_actions))
